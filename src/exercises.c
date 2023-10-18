ab8c156c611a3badf001d19bc83f98f4d38a911e
Yawar Hussain <yawarhussain793@gmail.com>
2023-10-19 02:44:55 +0530

    No 7: Create a variable name result_01 which hold the sum of nth term of even number and we use for loop to repetative checking even number

--- src/exercises.c
+++ src/exercises.c
@@ -105,7 +105,12 @@ int main(int argc, char **argv) {
        
   //7. Compute the summation of even numbers 1 thru n and print
   //   the result; example (n=5): 2 + 4 = 6
-
+        int reuslt_01 = 0;
+        for(int i = 1; i <= n; i++)
+        {
+          
+        }
+     
   //8. Write a solution to the following variation of the FizzBuzz
   //   problem.  Print out integers 1 thru n, one to a line, except
   //   that if the integer is divisible by 7 print "Foo" instead.
